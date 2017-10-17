e10855a5d5902762a2b3af11e28556c9a3b08729
Flerki <mairovichaa@yandex.ru>
2017-10-17 09:21:56 +0300

    separate files for distances and kernels

---
+++ kNN/distances.py
@@ -0,0 +1,8 @@
+# We can use different distance. Default - Euclidean distance
+minkowski_distance = lambda a, b, p: (abs((b.x - a.x) ** p) + abs((b.y - a.y) ** p)) ** (1 / p)
+
+distances = {
+    # 'manhattan': lambda a, b: minkowski_distance(a, b, 1),
+    'euclidian': lambda a, b: minkowski_distance(a, b, 2),
+    # 'third': lambda a, b: minkowski_distance(a, b, 3)
+}
---
+++ kNN/kernels.py
@@ -0,0 +1,12 @@
+
+import math
+
+
+# https://en.wikipedia.org/wiki/Kernel_(statistics)
+kernels = {
+    'epanechnikov': lambda val: 0.75 * (1 - val * val),
+    'gaussian': lambda val: 1 / math.sqrt(2 * math.pi) * math.exp((-1 / 2) * val * val),
+    # 'uniform': lambda val: 1 / 2,
+    # 'triangular': lambda val: 1 - abs(val),
+    # 'quartic': lambda val: 15 / 16 * (1 - val ** 2) ** 2
+}

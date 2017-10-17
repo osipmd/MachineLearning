e5828d9352162cf4c044c8f6f4e1b336d518988a
Flerki <mairovichaa@yandex.ru>
2017-10-17 09:25:34 +0300

    metrics in 3d

--- kNN/distances.py
+++ kNN/distances.py
@@ -6,3 +6,12 @@ distances = {
     'euclidian': lambda a, b: minkowski_distance(a, b, 2),
     # 'third': lambda a, b: minkowski_distance(a, b, 3)
 }
+
+minkowski_distance_3d = lambda a, b, p: (abs((b.x - a.x) ** p) + abs((b.y - a.y) ** p) + abs((b.z - a.z) ** p)) ** (
+1 / p)
+
+distances_3d = {
+    # 'manhattan': lambda a, b: minkowski_distance(a, b, 1),
+    'euclidian': lambda a, b: minkowski_distance(a, b, 2),
+    # 'third': lambda a, b: minkowski_distance(a, b, 3)
+}

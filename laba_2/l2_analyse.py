775965810b72ef8619f1a280ae6d7266850724fa
Flerki <mairovichaa@yandex.ru>
2017-10-30 16:36:34 +0300

    example of gradient descent

--- laba_2/l2_analyse.py
+++ laba_2/l2_analyse.py
@@ -1,20 +1,27 @@
 from l2_utils import *
 from analitic_regression import *
+from gradient_descent import *
 
 flats = read_data()
 
-coeffs = AnalyticRegression.calc(flats)
+# coeffs = AnalyticRegression.calc(flats)
+coeffs = GradientDescent.calc(flats)
+
+# to use had-written gradient calc function
+# coeffs = GradientDescent.calc(flats, True)
+
+print(coeffs)
 
 model = create_model(coeffs)
 
 y = list(map(lambda flat: flat.price, flats))
 predicted_y = list(map(lambda flat: model(flat), flats))
-
+#
 error = rms_error(y, predicted_y)
 print("RMS : ", error)
 
-while True:
-    area = int(input("area : "))
-    rooms = int(input("rooms : "))
-    flat = Flat(area, rooms)
-    print("predicted price : ", model(flat))
+# while True:
+#     area = int(input("area : "))
+#     rooms = int(input("rooms : "))
+#     flat = Flat(area, rooms)
+#     print("predicted price : ", model(flat))

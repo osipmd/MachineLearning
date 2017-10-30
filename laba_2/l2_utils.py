c0d968727551a380209bd2c74d35bfe7d32bd821
Flerki <mairovichaa@yandex.ru>
2017-10-30 12:44:22 +0300

    add to utils RMS and creation of model due to calculated coeffs

--- laba_2/l2_utils.py
+++ laba_2/l2_utils.py
@@ -9,3 +9,13 @@ def read_data(path_to_file='src_data/prices_without_head.txt'):
             point = Point(area, rooms, price)
             points.append(point)
     return points
+
+
+def rms_error(real, predicted):
+    def sqr_fun(elems): return (elems[0] - elems[1]) ** 2
+
+    l = len(real)
+    return sum(map(sqr_fun, zip(real, predicted))) / l
+
+def create_model(coeffs):
+    return lambda point: point.area * coeffs[0] + point.rooms * coeffs[1] + 1 * coeffs[2]

b647851b9e0a6bcf4bc1ea8c85fce387a73171ba
Flerki <mairovichaa@yandex.ru>
2017-10-31 09:10:12 +0300

    save all_coeffs

--- laba_2/gradient_descent.py
+++ laba_2/gradient_descent.py
@@ -2,8 +2,10 @@ import numpy as np
 
 
 class GradientDescent:
-    @staticmethod
-    def calc(data, eps=0.001, k=0.0000001, by_hand=False):
+    def __init__(self):
+        self.all_coeffs = []
+
+    def calc(self, data, eps=0.001, k=0.0000001, by_hand=False):
         w = np.array([0, 0, 0]).reshape(3, 1)
         x = np.array(list(map(lambda point: point.get_attrs_list(), data)))
         y = np.array(list(map(lambda point: [point.price], data)))
@@ -11,24 +13,24 @@ class GradientDescent:
 
         step = 0
         while w_prev is None or np.linalg.norm(w_prev - w) >= eps:
+            self.all_coeffs.append(w.reshape(1, 3)[0])
             step += 1
             w_prev = w
             eta = k / step
             if by_hand:
-                gradient = GradientDescent.calc_gradient_by_hand(data, w_prev)
+                gradient = self.calc_gradient_by_hand(data, w_prev)
             else:
-                gradient = GradientDescent.calc_gradient(x, w_prev, y)
+                gradient = self.calc_gradient(x, w_prev, y)
             w = w_prev - eta * gradient
 
+        self.all_coeffs.append(w.reshape(1, 3)[0])
         return w.reshape(1, 3)[0]
 
-    @staticmethod
-    def calc_gradient(x, w, y):
+    def calc_gradient(self, x, w, y):
         l = len(y)
         return 2 / l * x.transpose().dot(x.dot(w) - y)
 
-    @staticmethod
-    def calc_gradient_by_hand(data, w):
+    def calc_gradient_by_hand(self, data, w):
         l = len(data)
         dQ_dw0 = 2 / l * sum(
             map(lambda flat: flat.rooms * w.item(1) + flat.area * w.item(0) + w.item(2) - flat.price, data))
@@ -39,4 +41,3 @@ class GradientDescent:
             map(lambda flat: (flat.rooms * w.item(1) + flat.area * w.item(0) + w.item(2) - flat.price) * flat.rooms,
                 data))
         return np.array([dQ_dw1, dQ_dw2, dQ_dw0]).reshape(3, 1)
-
--- laba_2/l2_analyse.py
+++ laba_2/l2_analyse.py
@@ -1,16 +1,18 @@
-import math
-
 from l2_utils import *
 from analitic_regression import *
 from gradient_descent import *
-
+from l2_drawer import *
 flats = read_data()
 
-# coeffs = AnalyticRegression.calc(flats)
-coeffs = GradientDescent.calc(flats)
 
+# coeffs = AnalyticRegression.calc(flats)
+
+
+
+gd = GradientDescent()
+# coeffs = gd.calc(flats)
 # to use had-written gradient calc function
-# coeffs = GradientDescent.calc(flats, True)
+coeffs = gd.calc(flats, by_hand=True)
 
 print(coeffs)
 
@@ -18,10 +20,16 @@ model = create_model(coeffs)
 
 y = list(map(lambda flat: flat.price, flats))
 predicted_y = list(map(lambda flat: model(flat), flats))
-#
+
 error = rms_error(y, predicted_y)
 print("RMS : ", error)
 print("sqrt(RMS) : ", math.sqrt(error))
+
+errors = step_error(gd.all_coeffs, flats)
+
+Drawer().draw_step_error(errors)
+
+# print(list(map(lambda error: math.sqrt(error), step_error(gd.all_coeffs, flats))))
 
 # while True:
 #     area = int(input("area : "))

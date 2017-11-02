f8e6f6e78153c4c094c3ee9da0e8356911869392
Flerki <mairovichaa@yandex.ru>
2017-11-02 08:38:39 +0300

    add init for w from -1/2n to 1/2n

--- laba_2/gradient_descent.py
+++ laba_2/gradient_descent.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 
 
@@ -6,13 +8,18 @@ class GradientDescent:
         self.all_coeffs = []
 
     def calc(self, data, eps=0.001, k=0.0000001, by_hand=False):
-        w = np.array([0, 0, 0]).reshape(3, 1)
+        w1 = random.uniform(-1 / 6, 1 / 6)
+        w2 = random.uniform(-1 / 6, 1 / 6)
+        w0 = random.uniform(-1 / 6, 1 / 6)
+
+        w = np.array([w1, w2, w0]).reshape(3, 1)
         x = np.array(list(map(lambda point: point.get_attrs_list(), data)))
         y = np.array(list(map(lambda point: [point.price], data)))
         w_prev = None
 
+        MAX_STEP = 1500
         step = 0
-        while w_prev is None or np.linalg.norm(w_prev - w) >= eps:
+        while w_prev is None or np.linalg.norm(w_prev - w) >= eps and step < MAX_STEP:
             self.all_coeffs.append(w.reshape(1, 3)[0])
             step += 1
             w_prev = w
@@ -23,11 +30,12 @@ class GradientDescent:
                 gradient = self.calc_gradient(x, w_prev, y)
             w = w_prev - eta * gradient
 
-        self.all_coeffs.append(w.reshape(1, 3)[0])
+        # self.all_coeffs.append(w.reshape(1, 3)[0])
         return w.reshape(1, 3)[0]
 
     def calc_gradient(self, x, w, y):
         l = len(y)
+        # метод наименьших квадратов
         return 2 / l * x.transpose().dot(x.dot(w) - y)
 
     def calc_gradient_by_hand(self, data, w):

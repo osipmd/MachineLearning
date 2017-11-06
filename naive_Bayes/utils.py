161d72cde1ac4fc37607dd9e17516c6f249bf0f9
Mikhail Osipov <osipmd@yandex.ru>
2017-11-06 20:52:22 +0300

    Utils file for reading data from dir.

---
+++ naive_Bayes/utils.py
@@ -0,0 +1,47 @@
+import os
+
+
+
+
+def read_files_from_directory(number_of_file, dir_name=os.getcwd() + '/source_texts/part'):
+    absolutle_path = dir_name + str(number_of_file) + '/'
+    spam_count = 0
+    ham_count = 0
+
+    spam_dict = {}
+    ham_dict = {}
+
+    for filename in os.listdir(absolutle_path):
+        if 'spmsg' in filename:
+            spam_count += 1
+            spam_dict_from_file = read_file(absolutle_path + filename)
+            spam_dict = {k: spam_dict.get(k, 0) + spam_dict_from_file.get(k, 0)
+                         for k in set(spam_dict) | set(spam_dict_from_file)}
+        if 'legit' in filename:
+            ham_count += 1
+            ham_dict_from_file = read_file(absolutle_path + filename)
+            ham_dict = {k: ham_dict.get(k, 0) + ham_dict_from_file.get(k, 0)
+                        for k in set(ham_dict) | set(ham_dict_from_file)}
+    return spam_count, ham_count, spam_dict, ham_dict
+
+def read_file(filename):
+    i = 0
+    with open(filename) as file:
+        for line in file:
+            if i == 2:
+                dict = parse_line(line)
+                return dict
+            i += 1
+
+
+def parse_line(line):
+    dict = {}
+    words = map(int, line.split())
+    for word in words:
+        if word in dict:
+            dict[word] += 1
+        else:
+            dict[word] = 1
+    return dict
+
+

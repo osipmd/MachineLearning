d346a606a66fe771c7a2f574f2136d832dad06bf
Mikhail Osipov <osipmd@yandex.ru>
2017-11-09 23:20:06 +0300

    Add function for union of two dictionaries. Add subject of email for computation. It don't make result better.

--- naive_Bayes/utils.py
+++ naive_Bayes/utils.py
@@ -14,10 +14,8 @@ def read_dirs_statistics_except_dir(except_number_of_dir):
         spam_count, ham_count, spam_dict, ham_dict = read_files_from_directory(i + 1)
         global_spam_count += spam_count
         global_ham_count += ham_count
-        global_spam_dict = {k: global_spam_dict.get(k, 0) + spam_dict.get(k, 0)
-                            for k in set(global_spam_dict) | set(spam_dict)}
-        global_ham_dict = {k: global_ham_dict.get(k, 0) + ham_dict.get(k, 0)
-                           for k in set(global_ham_dict) | set(ham_dict)}
+        global_spam_dict = union_two_dict(global_spam_dict, spam_dict)
+        global_ham_dict = union_two_dict(global_ham_dict, ham_dict)
 
     return global_spam_count, global_ham_count, global_spam_dict, global_ham_dict
 
@@ -34,29 +32,42 @@ def read_files_from_directory(number_of_file, dir_name=os.getcwd() + '/source_te
         if is_filename_contains_spmsg(filename):
             spam_count += 1
             spam_dict_from_file = read_file(absolute_path + filename)
-            spam_dict = {k: spam_dict.get(k, 0) + spam_dict_from_file.get(k, 0)
-                         for k in set(spam_dict) | set(spam_dict_from_file)}
+            spam_dict = union_two_dict(spam_dict, spam_dict_from_file)
         if is_filename_contains_legit(filename):
             ham_count += 1
             ham_dict_from_file = read_file(absolute_path + filename)
-            ham_dict = {k: ham_dict.get(k, 0) + ham_dict_from_file.get(k, 0)
-                        for k in set(ham_dict) | set(ham_dict_from_file)}
+            ham_dict = union_two_dict(ham_dict, ham_dict_from_file)
     return spam_count, ham_count, spam_dict, ham_dict
 
 
 def read_file(filename):
     i = 0
+    dict = {}
     with open(filename) as file:
         for line in file:
+            if i == 0:
+                dict = parse_subject_line(line)
             if i == 2:
-                dict = parse_line(line)
-                return dict
+                dict = union_two_dict(dict, parse_line(line))
             i += 1
+    return dict
 
 
 def parse_line(line):
     dict = {}
     words = map(int, line.split())
+    for word in words:
+        if word in dict:
+            dict[word] += 1
+        else:
+            dict[word] = 1
+    return dict
+
+
+def parse_subject_line(line):
+    dict = {}
+    split_line = line.split()[1::]
+    words = map(int, split_line)
     for word in words:
         if word in dict:
             dict[word] += 1
@@ -77,3 +88,9 @@ def is_filename_contains_legit(filename):
         return True
     else:
         return False
+
+
+def union_two_dict(dict1, dict2):
+    union_dict = {k: dict1.get(k, 0) + dict2.get(k, 0)
+                  for k in set(dict1) | set(dict2)}
+    return union_dict
